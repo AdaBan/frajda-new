4f3a803baf615c0c516e32a2c5f9151734ab2b3e
AdaBan <ada.banaszak@gmail.com>
2022-04-20 13:14:33 +0200

    Minor tweaks to the 11ty base

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,17 @@ module.exports = config => {
   // Tell 11ty to use the .eleventyignore and ignore our .gitignore file
   config.setUseGitIgnore(false);
 
+  // Tell 11ty to open the browser on serve
+  config.setBrowserSyncConfig({
+    open: true
+  });
+
+  // Tell 11ty which files should trigger hot-reload on serve
+  config.addWatchTarget('./src/scss/');
+
+  // https://www.11ty.dev/docs/copy/
+  config.addPassthroughCopy('./src/assets/');
+
   return {
     markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
